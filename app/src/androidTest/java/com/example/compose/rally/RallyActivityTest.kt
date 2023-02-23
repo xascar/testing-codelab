13f92783a9c6ae9139669daf0b54458bd533432b
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-02-23 16:14:30 -0500

    Finishing codelab test

--- app/src/androidTest/java/com/example/compose/rally/RallyActivityTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/RallyActivityTest.kt
@@ -1,12 +1,46 @@
 package com.example.compose.rally
 
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createComposeRule
+import com.example.compose.rally.ui.overview.OverviewBody
 import org.junit.Assert.*
+import org.junit.Rule
 
 import org.junit.Test
 
 class RallyActivityTest {
 
+    @get:Rule
+    val composeTestRule = createComposeRule()
+
     @Test
-    fun rallyApp() {
+    fun rallyApp_selectionChangedOnClickingTabs() {
+        composeTestRule.setContent {
+            RallyApp()
+        }
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("selectionChangedOnClickingTabs")
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Overview .name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Overview .name)
+                        ),
+                useUnmergedTree = true
+            ).performClick()
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Overview.name.uppercase()) and
+                        hasParent(
+                            hasContentDescription(RallyScreen.Overview.name)
+                        ),
+                useUnmergedTree = true
+            )
+            .assertIsSelected()
+
     }
+
+
 }
