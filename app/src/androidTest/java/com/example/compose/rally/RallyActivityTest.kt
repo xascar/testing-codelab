c92c936b154c9e8a1d6c8a5013a2a7e6add6a98c
Oscar Gustavo Moreno Bautista <xascar94@gmail.com>
2023-02-23 21:10:41 -0500

    Optional testing

--- app/src/androidTest/java/com/example/compose/rally/RallyActivityTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/RallyActivityTest.kt
@@ -2,43 +2,97 @@ package com.example.compose.rally
 
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
-import com.example.compose.rally.ui.overview.OverviewBody
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Assert.*
+import org.junit.Before
 import org.junit.Rule
-
 import org.junit.Test
+import org.junit.runner.RunWith
 
+/**
+ *   Q1: Should I need to split into smaller functions or keep all in one function?
+ *   Q2: Is there any way to print just the portion I'm testing for the Semantics tree?   (Not to include Text = '[Car Savings]'logged in details)
+ */
+
+@RunWith(AndroidJUnit4::class)  // To indicate that we've to run it with AndroidJUnit4 runner
 class RallyActivityTest {
 
     @get:Rule
     val composeTestRule = createComposeRule()
 
-    @Test
-    fun rallyApp_selectionChangedOnClickingTabs() {
-        composeTestRule.setContent {
+    @Before
+    fun setUp() {
+        composeTestRule.setContent {    // setting our composable as content for test
             RallyApp()
         }
+    }
 
-        composeTestRule.onRoot(useUnmergedTree = true).printToLog("selectionChangedOnClickingTabs")
+    @Test
+    fun rallyApp_overviewExistAndIsSelected() {
 
-        composeTestRule
-            .onNode(
-                hasText(RallyScreen.Overview .name.uppercase()) and
-                        hasParent(
-                            hasContentDescription(RallyScreen.Overview .name)
-                        ),
-                useUnmergedTree = true
-            ).performClick()
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("rallyApp_overviewExistAndIsSelected")
 
         composeTestRule
             .onNode(
                 hasText(RallyScreen.Overview.name.uppercase()) and
                         hasParent(
-                            hasContentDescription(RallyScreen.Overview.name)
+                                    isSelected()
                         ),
                 useUnmergedTree = true
-            )
-            .assertIsSelected()
+            ).assertExists()
+
+    }
+
+    @Test
+    fun rallyApp_accountsTabIsNotSelected() {
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("rallyApp_accountsTabIsNotSelected")
+
+        composeTestRule
+            .onNode(
+                hasContentDescription(RallyScreen.Accounts .name) and
+                        isNotSelected(),
+                useUnmergedTree = true
+            ).assertExists()
+
+    }
+
+    @Test
+    fun rallyApp_tabChangedWhenClickPerformed() {
+        rallyApp_overviewExistAndIsSelected()
+        rallyApp_accountsTabIsNotSelected()
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("rallyApp_tabChangedWhenClickPerformed")
+
+        composeTestRule
+            .onNode(
+                hasContentDescription(RallyScreen.Accounts .name) and
+                        isNotSelected(),
+                useUnmergedTree = true
+            ).performClick()
+
+        Thread.sleep(3000)
+
+        composeTestRule
+            .onNode(
+                hasText(RallyScreen.Accounts.name.uppercase()) and
+                        hasParent(
+                            isSelected()
+                        ),
+                useUnmergedTree = true
+            ).assertExists()
+
+        Thread.sleep(3000)
+
+        composeTestRule
+            .onNode(
+                hasContentDescription(RallyScreen.Overview .name) and
+                        isNotSelected(),
+                useUnmergedTree = true
+            ).assertExists()
+
+        composeTestRule.onRoot(useUnmergedTree = true).printToLog("rallyApp_tabChangedWhenClickPerformed")
+
 
     }
 
--- app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
+++ app/src/androidTest/java/com/example/compose/rally/TopAppBarTest.kt
@@ -3,6 +3,7 @@ package com.example.compose.rally
 import androidx.compose.ui.test.*
 import androidx.compose.ui.test.junit4.createComposeRule
 import com.example.compose.rally.ui.components.RallyTopAppBar
+import com.example.compose.rally.ui.theme.RallyTheme
 import org.junit.Rule
 import org.junit.Test
 
@@ -17,11 +18,13 @@ class TopAppBarTest {
         val allScreens = RallyScreen.values().toList()
 
         composeTestRule.setContent {
-            RallyTopAppBar(
-                allScreens = allScreens,
-                onTabSelected = {},
-                currentScreen = RallyScreen.Accounts
-            )
+            RallyTheme{
+                RallyTopAppBar(
+                    allScreens = allScreens,
+                    onTabSelected = {},
+                    currentScreen = RallyScreen.Accounts
+                )
+            }
         }
         Thread.sleep(5000)
 
